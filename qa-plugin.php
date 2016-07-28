05b18ab3c8f494bab583684662de52e067226dd0
Arjun <arjunsuresh1987@gmail.com>
2016-07-28 21:42:13 +0000

    layer removed

--- qa-plugin.php
+++ qa-plugin.php
@@ -7,9 +7,6 @@
 			exit;
 	}
 	
-	qa_register_plugin_layer('qa-filtertags-layer.php', 'Filter Tags Layer');	
-	
-
 
 	qa_register_plugin_module('module', 'qa-filtertags-admin.php', 'qa_filtertags_admin', 'Filter Tags Admin');
 	
